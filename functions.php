d9d2b6b05311ea556dc06f9cb6e1e8e79286ec37
hkdeven <deven@codefll.org>
2016-10-21 11:26:34 -0400

    Updated script with GSAP animations.

--- functions.php
+++ functions.php
@@ -1,7 +1,14 @@
 <?php
-function my_theme_enqueue_styles() {
 
-    $parent_style = 'parent-style';// This is the parent theme 'Blade'
+function theme_gsap_script() {  // This loads GSAP - used for animations
+    wp_enqueue_script( 'gsap-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js', array(), false, true );
+    wp_enqueue_script( 'gsap-js', get_template_directory_uri() . '/wp-content/js/custom-gsap-scripts.js', array(), false, true );
+}
+add_action( 'wp_enqueue_scripts', 'theme_gsap_script' );
+
+function my_theme_enqueue_styles() {  // This loads the js from the parent theme 'Blade' - AFTER all other js listed here
+
+    $parent_style = 'parent-style';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
