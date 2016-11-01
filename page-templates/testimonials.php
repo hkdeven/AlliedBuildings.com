101948832038906dae06719a7e836d97d2881956
hkdeven <deven@codefll.org>
2016-11-01 11:35:00 -0400

    Testimonial page template added.

---
+++ page-templates/testimonials.php
@@ -0,0 +1,130 @@
+<?php
+/*
+Template Name: FAQ
+*/
+?>
+<?php get_header(); ?>
+
+<?php the_post(); ?>
+		<!-- CONTENT -->
+		<div id="grve-content" class="clearfix">
+			<div class="grve-content-wrapper">
+				<!-- MAIN CONTENT -->
+				<div id="grve-main-content">
+					<div class="grve-main-content-wrapper clearfix">
+
+						<!-- PAGE CONTENT -->
+						<div id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<?php the_content(); ?>
+							<!-- PAGE CONTENT -->
+<!-- <div id="page-177" class="post-177 page type-page status-publish hentry"> -->
+<div class="grve-section grve-fullwidth-background grve-bg-none">  <div class="grve-container">    <div class="grve-row grve-bookmark">
+<div class="wpb_column grve-column-2-3">
+<div class="grve-element grve-box grve-align-left" style=""><div class="grve-media"><iframe width="1080" height="608" src="https://www.youtube.com/embed/fN3DoJReQMA?feature=oembed" frameborder="0" allowfullscreen></iframe></div>  <div class="grve-box-content">    <p></p>  </div></div>
+</div>
+
+<div class="wpb_column grve-column-1-3">
+<div class="grve-element grve-box grve-align-left" style=""><div class="grve-media"><iframe width="1080" height="608" src="https://www.youtube.com/embed/hDHFaC05Qlw?feature=oembed" frameborder="0" allowfullscreen></iframe></div>  <div class="grve-box-content">    <p></p>  </div></div><div class="grve-element grve-box grve-align-left" style=""><div class="grve-media"><iframe width="1080" height="608" src="https://www.youtube.com/embed/i9GsejhrCxY?feature=oembed" frameborder="0" allowfullscreen></iframe></div>  <div class="grve-box-content">    <p></p>  </div></div>
+</div>
+
+<div class="wpb_column grve-column-1">
+<div class="vc_empty_space"  style="height: 70px" ><span class="vc_empty_space_inner"></span></div>
+<div class="grve-element grve-with-gap grve-testimonial-grid grve-isotope" style="" data-columns="3" data-columns-tablet-landscape="3" data-columns-tablet-portrait="3" data-columns-mobile="1" data-layout="fitRows" data-gutter-size="40">
+
+	<div class="grve-isotope-container">
+
+<div class="grve-isotope-item grve-testimonial-item">
+<div class="grve-isotope-item-inner grve-zoomIn">
+<div class="grve-testimonial-element grve-style-2 grve-with-shadow">
+<div class="grve-container">
+	<div class="grve-container"><p>&#8220;We needed steel and siding for a indoor horse arena at 7000ft with a 90 pound snow load. After researching other companies Allied Steel Buildings had the materials that best fit our needs. They were very helpful and great to work. I highly recommend them.&#8221;</p>
+</div>							</div>
+<div class="grve-testimonial-author grve-border grve-border-top">
+																	<div class="grve-small-text grve-heading-color grve-testimonial-name">E. Gouveia, Colorado</div>
+								</div>
+</div>
+</div>
+</div>
+
+<div class="grve-isotope-item grve-testimonial-item">
+<div class="grve-isotope-item-inner grve-zoomIn">
+<div class="grve-testimonial-element grve-style-2 grve-with-shadow">
+<div class="grve-container">
+	<div class="grve-container"><p>&#8220;Allied Steel Buildings was terrific to work with from start to finish. Top quality performance and unmatched expertise!!!&#8221;</p>
+</div>							</div>
+<div class="grve-testimonial-author grve-border grve-border-top">
+																	<div class="grve-small-text grve-heading-color grve-testimonial-name">Wellesley Building Company LLC, New York</div>
+								</div>
+</div>
+</div>
+</div>
+
+<div class="grve-isotope-item grve-testimonial-item">
+<div class="grve-isotope-item-inner grve-zoomIn">
+<div class="grve-testimonial-element grve-style-2 grve-with-shadow">
+<div class="grve-container">
+	<div class="grve-container"><p>&#8220;We needed steel and siding for a indoor horse arena at 7000ft with a 90 pound snow load. After researching other companies Allied Steel Buildings had the materials that best fit our needs. They were very helpful and great to work. I highly recommend them.&#8221;</p>
+</div>							</div>
+<div class="grve-testimonial-author grve-border grve-border-top">
+																	<div class="grve-small-text grve-heading-color grve-testimonial-name">E. Gouveia, Utah</div>
+								</div>
+</div>
+</div>
+</div>
+
+<div class="grve-isotope-item grve-testimonial-item">
+<div class="grve-isotope-item-inner grve-zoomIn">
+<div class="grve-testimonial-element grve-style-2 grve-with-shadow">
+<div class="grve-container">
+	<div class="grve-container"><p>&#8220;Very happy with the service I received. The customer service was outstanding, the personnel was professional and very knowledgeable of their products. I will definitely be recommending Allied Steel Building. Thank you for the GREAT service.&#8221;</p>
+</div>							</div>
+<div class="grve-testimonial-author grve-border grve-border-top">
+																	<div class="grve-small-text grve-heading-color grve-testimonial-name">D. Rodriguez, North Carolina</div>
+								</div>
+</div>
+</div>
+</div>
+
+<div class="grve-isotope-item grve-testimonial-item">
+<div class="grve-isotope-item-inner grve-zoomIn">
+<div class="grve-testimonial-element grve-style-2 grve-with-shadow">
+<div class="grve-container">
+	<div class="grve-container"><p>&#8220;We are dealing with Allied for the past 6 years and throughout all our dealings we were treated with the upmost professionalism and service. We recommend that anyone who needs a quality and competitively priced Prefab steel structure should go to Allied Steel for their project!&#8221;</p>
+</div>							</div>
+<div class="grve-testimonial-author grve-border grve-border-top">
+																	<div class="grve-small-text grve-heading-color grve-testimonial-name">M. Glatzer, Florida</div>
+								</div>
+</div>
+</div>
+</div>
+
+<div class="grve-isotope-item grve-testimonial-item">
+<div class="grve-isotope-item-inner grve-zoomIn">
+<div class="grve-testimonial-element grve-style-2 grve-with-shadow">
+<div class="grve-container">
+	<div class="grve-container"><p>&#8220;We built a 50&#215;80 with a 40 ft scissor door as a hangar for my airplanes on a farm in Montana. Unbelievably strong building. Assembly was fast and straightforward. Great people to deal with.&#8221;</p>
+</div>							</div>
+<div class="grve-testimonial-author grve-border grve-border-top">
+																	<div class="grve-small-text grve-heading-color grve-testimonial-name">J. Lefore, Alberta</div>
+								</div>
+</div>
+</div>
+</div>
+
+			</div>
+</div>
+
+<div class="grve-element grve-align-left"><a class="grve-btn grve-btn-extralarge grve-square grve-bg-primary-1 grve-bg-hover-black grve-btn-line grve-fullwidth-btn grve-fluid-btn-short" href="#" target="_self" style=""><span>SEE MORE</span></a></div>
+</div>
+</div>  </div>  <div class="grve-background-wrapper">  </div></div>
+<!-- </div> -->
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+<?php get_footer();
+
+//Omit closing PHP tag to avoid accidental whitespace output errors.
